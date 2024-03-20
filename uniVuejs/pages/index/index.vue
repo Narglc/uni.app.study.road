c7118a7cc08ef66d83d7f9c2d3ab3dec8455a9bc
Narglc <qingcheliuzhi@qq.com>
2024-03-20 10:58:03 +0800

    模板语法

--- uniVuejs/pages/index/index.vue
+++ uniVuejs/pages/index/index.vue
@@ -1,52 +1,31 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view>a+3:{{2+3}}</view>
+	<view>a: {{a}}</view>
+	<view>a+5: {{a+5}}</view>
+	<view>
+		时间戳：{{Date.now()}}
+	</view>
+	<view>
+		随机数:{{Math.random()}}
+	</view>
+	<view>
+		{{ 1 < 2 ? "张三":"李四"}}
+	</view>
+	<view>
+		function: {{ testFn()}}
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+<script setup>
+// 组合式API
+const a = 6;
 
-		},
-		methods: {
+function testFn(){
+	return "vue3 学习!!!"
+}
 
-		}
-	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
