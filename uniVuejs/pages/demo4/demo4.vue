f375319bab94c070540c1a82ef3c7fe5761b5221
Narglc <qingcheliuzhi@qq.com>
2024-03-20 13:52:49 +0800

    事件处理 v-on 简写 @

---
+++ uniVuejs/pages/demo4/demo4.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<button type="primary" v-on:click="onClick">你点击了{{ count }} 次!</button>
+		
+		<button type="primary" @click="onClick" :style="{background:color}">
+			你点击了{{ count }} 次!
+		</button>
+	</view>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+let count = ref(0);
+
+function onClick(){
+	count.value++;
+	color.value = "#" + String(Math.random()).substring(3,9)
+}
+
+const color = ref("#fc359a")
+
+</script>
+
+<style>
+
+</style>
