b6c1395107609894a2044360ef0030bf279a4050
Narglc <qingcheliuzhi@qq.com>
2024-03-20 15:51:39 +0800

    视频给的模拟京东商城

--- uniVuejs/pages/jdstore/jdstore.vue
+++ uniVuejs/pages/jdstore/jdstore.vue
@@ -1,41 +1,47 @@
 <template>
 	<view class="out">
-		<template v-for="(item,index) in shoplist">
-			<view class="item">
-				<checkbox checked="false" @change="changeDelList"></checkbox>
-				<view>{{item.name}}</view>
-				<view>{{item.price}}</view>
-				<button type="default" @click="onDelete">删除</button>
-			</view>
-		</template>
+		<view class="item" v-for="(item,index) in goods" :key="item.id">
+			<checkbox></checkbox>
+			<text class="title">{{item.name}}</text>
+			<text class="price">{{item.price}}</text>
+			<text class="del" @click="remove(index)">删除</text>
+		</view>
 	</view>
 </template>
 
 <script setup>
 import {ref} from "vue";
-const shoplist = ref([
-	{name:"第一个",price:98},
-	{name:"剃须刀", price:300}
+const goods = ref([
+	{id:11, name:"小米汽车",price:298888},
+	{id:12,name:"剃须刀", price:300},
+	{id:13,name:"苹果", price:2},
+	{id:14,name:"韭菜", price:0.1}
 ])
 
 const delList = ref([])
 
-function onDelete(e){
-	console.log(e);
+function remove(idx){
+	console.log(idx);
+	goods.value.splice(idx,1)
 }
-
-function changeDelList(e){
-	console.log(e);
-	// if(e.detail.value){
-	// 	onDelete.push()
-	// }
-}
-
 </script>
 
 <style lang="scss" scoped>
-.item{
-	display: flex;
-	border: 1px solid red;
+.out{
+	padding: 12px;
+	.item{
+		padding: 10px 0;
+		border: 1px dotted red;
+		margin-bottom: 4px;
+		.price{
+			margin-left: 20px;
+		}
+		.del{
+			color: #c00;
+			margin-left: 30px;
+		}
+	}	
 }
+	
+
 </style>
