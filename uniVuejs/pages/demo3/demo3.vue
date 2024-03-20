292897bcda07631b819d5bea7fde7f242a5db1d5
Narglc <qingcheliuzhi@qq.com>
2024-03-20 12:11:46 +0800

    v-bind 2

--- uniVuejs/pages/demo3/demo3.vue
+++ uniVuejs/pages/demo3/demo3.vue
@@ -4,6 +4,18 @@
 		<image :src="arrs[idx]" mode="aspectFill" class = "randomPic"></image>
 		
 		<button type="primary" :disabled="buttonDisable" @click="clickHandle">按钮</button>
+		
+		<view class="box active" :class="{bigger:true,'hightHandle':ishightHandle}">
+			<text>v-bind指令 [绑定对象]</text>
+		</view>
+		
+		<view class="box" :class="isActive?'active':''">
+			另一个box... [常用三元表达式]
+		</view>
+		
+		<view class="box" :style="{width: '500px',height:'50px', fontSize: fontSize +'px'}">
+			内联样式
+		</view>
 	</view>
 </template>
 
@@ -17,7 +29,9 @@ let idx = ref(0)
 
 setInterval(()=>{
 	idx.value = (idx.value + 1) %3
-},2000)
+	isActive.value = !isActive.value
+	fontSize.value++
+},1000)
 
 let buttonDisable = false
 
@@ -25,6 +39,11 @@ function clickHandle(){
 	console.log("点击了按钮");
 }
 
+let ishightHandle = ref(true)
+
+let isActive = ref(false)
+
+let fontSize = ref(10)
 </script>
 
 <style lang="scss">
@@ -39,5 +58,24 @@ function clickHandle(){
 	display: flex;
 	margin-left: auto;
 	margin-right: auto;
+}
+
+.box{
+	width: 200px;
+	height: 200px;
+	background: orange;
+	font-size: 20px;
+}
+.active{
+	background: green;
+	color: #FFF;
+}
+
+.bigger{
+	width: 400px;
+}
+
+.hightHandle{
+	height: 100px;
 }
 </style>
