81cc9f809d48d0eac82a42f01eff0d23ca6fe675
Narglc <qingcheliuzhi@qq.com>
2024-03-21 18:19:52 +0800

    同时监听多个变量

--- uniVuejs/pages/demo10/demo10.vue
+++ uniVuejs/pages/demo10/demo10.vue
@@ -6,11 +6,12 @@
 		<view>[计算属性2]全称: {{ fullNameComputed2 }}</view>
 		<view>[方法]全称: {{ fullNameFunc() }}</view>
 		<view>[方法2]全称: {{ fullNameFunc2() }}</view>
+		<view>[侦听器]全称: {{fullNameWatch}}</view>
 	</view>
 </template>
 
 <script setup>
-import {ref,computed} from "vue";
+import {ref,computed,watch} from "vue";
 
 const firstName = ref("");
 const lastName = ref("");
@@ -37,6 +38,23 @@ const fullNameFunc2 = ()=>firstName.value + " - " + lastName.value
  *		计算属性是有缓存的，同一页面多次调用 计算属性，只执行一次；
  * 		方法会计算多次，同一页面多次调用时，每调用一次方法就被执行一次
  */	
+
+// 侦听器
+const fullNameWatch = ref("")
+
+// 分别监听两个变量[分开监听更可读]
+// watch(firstName,newVal=>{
+// 	fullNameWatch.value = newVal + "-" + lastName.value;
+// })
+
+// watch(lastName,newVal=>{
+// 	fullNameWatch.value = firstName.value + "-" + newVal;
+// })
+
+// 后者同时监听两个变量
+watch([firstName,lastName],([Nfirstname,NlastName],[OfirstName,OlastName])=>{
+	fullNameWatch.value = Nfirstname + "-" + NlastName;
+})
 
 </script>
 
