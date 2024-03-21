fd43b7fd201e7a8ef7beaf0ad00cf0cbbbfc5150
Narglc <qingcheliuzhi@qq.com>
2024-03-21 18:40:39 +0800

    两种components命名方式

---
+++ uniAppComponents/components/jac-header/jac-header.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"jac-header",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
