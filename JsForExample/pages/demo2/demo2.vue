17703435695ee07cf06738842eb75cd447ae8c88
Narglc <qingcheliuzhi@qq.com>
2024-03-21 11:03:36 +0800

    js的for,forEach对比

---
+++ JsForExample/pages/demo2/demo2.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="out">
+		<view>Test</view>
+	</view>
+</template>
+
+<script setup>
+
+
+// uniapp 发起请求演示
+// uni.request({
+// 		url:"http://jsonplaceholder.typicode.com/posts/1"
+// }).then(res=>{
+// 	console.log(res);
+// })
+
+// forEach 是不支持异步等待的， 以下异步请求会有问题
+// let arrs = [1,2,3] //Array.from({length:3},(_,index)=>index+1);
+// let datas=[];
+// arrs.forEach(async item=>{
+// 	const {data} = await uni.request({
+// 		url:"http://jsonplaceholder.typicode.com/posts/"+item
+// 	})
+// 	console.log("rsp_data:",data);
+// 	datas.push(data)
+// })
+// console.log(datas);
+
+// 正确的写法:
+// for循环支持await 异步等待
+async function fetchData(){
+	let arrs = Array.from({length:3},(_,index)=>index+1);
+	let datas = [];
+	for (let i = 0; i < arrs.length; i++) {
+		const {data} = await uni.request({
+			url:"http://jsonplaceholder.typicode.com/posts/"+arrs[i]
+		})
+		console.log("rsp_data:",data);
+		datas.push(data)	
+	}
+	console.log("datas:",datas);
+}
+
+fetchData();
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ JsForExample/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="content">
+		<view>for的基本使用</view>
+	</view>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const title = ref("hello world")
+
+let arrs = [...Array(9999999).keys()]
+// console.log(arrs);
+
+let total = 0;
+let starTime = Date.now();
+
+/*
+for (let i = 0; i < arrs.length; i++)	total+=i;
+
+for (let i = 0; i < arrs.length; i++) {
+	if(i==100) break;
+	total += i;
+}
+*/
+
+// forEach 使用, 针对数组中的每一项进行处理
+// forEach 中不支持break,
+// 只支持return, 但功能却相当于for-continue
+arrs.forEach(item=>{
+	if(item == 10) return;
+	total += item;
+})
+
+let endTime = Date.now();
+
+let countTime = endTime - starTime;
+console.log("计数:"+total);
+console.log("消耗时间:"+countTime);
+
+</script>
+
+<style lang="scss">
+
+</style>
