f9e04730905aaf8e5a625d48cbf262df2d62e794
Narglc <qingcheliuzhi@qq.com>
2024-03-22 15:48:53 +0800

    组件生命周期onMounted,onUnmounted的使用

---
+++ uniAppComponents/components/life-demo/life-demo.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<view>子组件生命周期</view>
+	</view>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue';
+
+onUnmounted(()=>{
+	console.log("子组件被卸载!!");
+})
+
+onMounted(()=>{
+	console.log("子组件被挂载了~");
+})
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ uniAppComponents/pages/demo4/demo4.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="layout">
+		<scroll-view scroll-x scroll-y="true" class="scroll" ref="scroll">
+			<view></view>
+		</scroll-view>
+	</view>
+	<view>
+		<life-demo v-if="show"></life-demo>
+	</view>
+</template>
+
+<script setup>
+// 如何获取DOM节点
+import {onMounted, ref} from "vue";
+const scroll = ref(null);
+
+const show = ref(true)
+
+// 在挂载结束时获取
+onMounted(()=>{
+	console.log(scroll.value);
+})
+
+setInterval(()=>{
+	show.value = !show.value;
+},3000)
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
