3743dde28c8f1d7d6a6f277a75273284d54c00b7
Narglc <qingcheliuzhi@qq.com>
2024-03-22 16:16:18 +0800

    向外导出defineExpose:支持传值，传方法

---
+++ uniAppComponents/components/child-demo/child-demo.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="out">
+		子组件 count值:{{count}}
+	</view>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const count = ref(100);
+
+const updateCount = function(){
+	count.value++;
+}
+
+// 向外导出
+defineExpose({
+	count,
+	str:"narglc",
+	updateCount
+})
+
+</script>
+
+<style lang="scss" scoped>
+.out{
+	padding: 20px;
+	background: #eee;
+}
+</style>
---
+++ uniAppComponents/pages/demo5/demo5.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="layout">
+		<child-demo ref="child"></child-demo>
+		<view>-----</view>
+		<button @click="update">点击修改子值</button>
+	</view>
+</template>
+
+<script setup>
+import {onMounted, ref} from "vue";
+
+const child = ref(null);
+
+console.log(child.value);
+
+onMounted(()=>{
+	console.log(child.value);
+})
+
+function update(){
+	child.value.updateCount();
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
