ef2802df28b6507fc7e66843111757fb102387bb
Narglc <qingcheliuzhi@qq.com>
2024-03-22 15:02:02 +0800

    子传父事件 ('add',123) -> @add='onAdd'

---
+++ uniAppComponents/components/jac-child/jac-child.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>
+		子组件
+		<button @click="$emit('add',Math.random())">按钮</button>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ uniAppComponents/pages/demo3/demo3.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="">
+		<jac-child @add="onAdd"></jac-child>
+		<view class="box" :style="{background:color}">{{num}}</view>
+	</view>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const num = ref(0);
+const color = ref("#ccc")
+
+// const onAdd = function(num){
+// 	console.log(num);
+// }
+
+// 或者写为:
+function onAdd(e){
+	// console.log(e);
+	num.value = e;
+	color.value = "#" + String(e).substring(3,9);
+	// console.log(color.value);
+}
+</script>
+
+<style lang="scss" scoped>
+.box{
+	width: 80%;
+	height: 50px;
+	margin: 20px;
+	margin-left: auto;
+	margin-right: auto;
+	// border-radius: 50%;
+	text-align: center;
+}
+</style>
