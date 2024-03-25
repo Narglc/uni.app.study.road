2e3f89a393acd0ce571165e01a88af31ec8270c8
Narglc <qingcheliuzhi@qq.com>
2024-03-25 18:05:31 +0800

    uni.request的使用方法1

---
+++ globalFile/pages/demo4/demo4.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="layout">
+<!-- 		<view v-for="(item,index) in rspData" :key="item.id">
+			index:{{index}},id:{{item.id}}->title:{{item.title}}
+		</view> -->
+		<view class="row" v-for="(item,index) in rspData" :key="item.id">
+			<view class="title">标题:{{item.title}}</view>
+			<view class="content">内容:{{item.body}}</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+import {ref} from "vue";
+let rspData = ref([])
+	
+function request(){
+	uni.request({
+		url:"https://jsonplaceholder.typicode.com/posts",
+		success: res => {
+			console.log(res);
+			rspData.value = res.data;
+		}
+	})
+}
+
+request()
+
+</script>
+
+<style lang="scss" scoped>
+.layout{
+	padding: 30rpx;
+	.row{
+		border-bottom: 1px solid #cfcfcf;
+		padding: 20rpx 0;
+		.title{
+			font-size: 36rpx;
+			margin-bottom: 10rpx;
+		}
+		.content{
+			font-size: 28rpx;
+			color: #666;
+		}
+	}
+}
+</style>
