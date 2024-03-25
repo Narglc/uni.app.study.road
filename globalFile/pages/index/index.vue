f3a3d785afa79013496e8cedc0eac1efc54c63af
Narglc <qingcheliuzhi@qq.com>
2024-03-25 15:48:29 +0800

    uni.hideToast 弹窗隐藏

--- globalFile/pages/index/index.vue
+++ globalFile/pages/index/index.vue
@@ -1,6 +1,8 @@
 <template>
 	<view class="content">
 		<navigator open-type="reLaunch" url="/pages/user/user">用户</navigator>
+		<button @click="show">显示</button>
+		<button @click="hide">隐藏</button>
 		<image class="logo" src="/static/images/logo.png"></image>
 		<view class="" v-for="item in 100">
 			{{ item }}
@@ -15,16 +17,24 @@ onReachBottom(()=>{
 	console.log("到底了！！");
 })
 
-uni.showToast({
-	title:"失败失败失败一次两",	// title在 icon=success时最多可以显示7个汉字长度，想要显示更多时可设置icon=none
-	// icon:"none"		// success, fail
-	"image":"../../static/images/xxmLogo.png",
-	mask:true,
-	duration:3000,
-	success:res=>{
-		console.log(res);
-	}
-})
+function show(){
+	uni.showToast({
+		title:"失败失败失败一次两",	// title在 icon=success时最多可以显示7个汉字长度，想要显示更多时可设置icon=none
+		// icon:"none"		// success, fail
+		"image":"../../static/images/xxmLogo.png",
+		// mask:true,
+		// duration:3000,
+		success:res=>{
+			console.log(res);
+		}
+	})
+}
+
+function hide(){
+	uni.hideToast()
+}
+
+
 
 </script>
 
