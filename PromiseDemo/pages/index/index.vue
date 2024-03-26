548e49bd552b86da1d30c3a44f05ceac932df15d
Narglc <qingcheliuzhi@qq.com>
2024-03-26 18:18:28 +0800

    传统型的回调函数做回调处理: 封装uni.request请求

--- PromiseDemo/pages/index/index.vue
+++ PromiseDemo/pages/index/index.vue
@@ -8,10 +8,59 @@
 import {onLoad} from  "@dcloudio/uni-app"
 
 onLoad(()=>{
-	getData()
+	getNav(res=>{
+		console.log(res);
+		let id = res.data[0].id;
+		getList(id, res=>{
+			console.log(res);
+			let aid = res.data[0].id;
+			getComments(aid, res=>{
+				console.log(res);
+			})
+		})
+	})
+	
+	
 })
 
-function getData(){
+// 获取分类列表id
+function getNav(callback){
+	uni.request({
+		url:"https://ku.qingnian8.com/dataApi/news/navlist.php",
+		success:res=>{
+		  callback(res);
+		},
+	})
+}
+
+// 根据分类id获取该分类下的所有文章
+function getList(id, callback){
+      uni.request({
+        url:"https://ku.qingnian8.com/dataApi/news/newslist.php",
+        data:{
+          cid:id
+        },
+		success:res=>{
+		  callback(res)
+		},
+	})
+}
+
+// 根据文章id找到该文章下的评论
+function getComments(id, callback){
+	uni.request({
+	  url:"https://ku.qingnian8.com/dataApi/news/comment.php",
+	  data:{
+	    aid:id
+	  },
+	  success:res=>{
+		callback(res)
+	  }
+	})
+}
+
+
+/* function getData(){
   //获取分类列表id
   uni.request({
     url:"https://ku.qingnian8.com/dataApi/news/navlist.php",
@@ -43,7 +92,7 @@ function getData(){
 
     }
   })
-}
+} */
 
 </script>
 
