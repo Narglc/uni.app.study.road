0a802f43e84778be4afce4557c68bd4ea44a4135
Narglc <qingcheliuzhi@qq.com>
2024-04-02 16:30:07 +0800

    壁纸项目:用户中心

--- wallpaper-kt/pages/user/user.vue
+++ wallpaper-kt/pages/user/user.vue
@@ -1,6 +1,72 @@
 <template>
-	<view class="">
-		用户中心
+	<view class="useLayout">
+		<view class="userInfo">
+			<view class="avatar">
+				<image src="../../static/xxmLogo.png" mode="aspectFill"></image>
+			</view>
+			<view class="ip">100.100.100.100</view>
+			<view class="address">来自于 山东</view>
+		</view>
+		
+		<view class="section">
+			<view class="list">
+				<view class="row">
+					<view class="left">
+						<uni-icons type="download-filled" size="20" color="#28B389"></uni-icons>
+						<view class="text">我的下载</view>
+					</view>
+					<view class="right">
+						<view class="text">33</view>
+						<uni-icons type="right" size="10" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="star-filled" size="20" color="#28B389"></uni-icons>
+						<view class="text">我的评分</view>
+					</view>
+					<view class="right">
+						<view class="text">33</view>
+						<uni-icons type="right" size="10" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="chatboxes-filled" size="20" color="#28B389"></uni-icons>
+						<view class="text">联系客服</view>
+					</view>
+					<view class="right">
+						<view class="text"></view>
+						<uni-icons type="right" size="10" color="#aaa"></uni-icons>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+		<view class="section">
+			<view class="list">
+				<view class="row">
+					<view class="left">
+						<uni-icons type="notification-filled" size="20" color="#28B389"></uni-icons>
+						<view class="text">订阅更新</view>
+					</view>
+					<view class="right">
+						<view class="text"></view>
+						<uni-icons type="right" size="10" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="flag-filled" size="20" color="#28B389"></uni-icons>
+						<view class="text">常见更新</view>
+					</view>
+					<view class="right">
+						<view class="text"></view>
+						<uni-icons type="right" size="10" color="#aaa"></uni-icons>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -9,5 +75,68 @@
 </script>
 
 <style lang="scss" scoped>
+.useLayout{
+	.userInfo{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;			// 排列方向
+		padding: 50rpx 0;
+		.avatar{
+			width: 160rpx;
+			height: 160rpx;
+			border-radius: 50%;
+			overflow: hidden;			// 给父级增加圆角时，必须要使用overflow:hidden
+			image{
+				width: 100%;
+				height: 100%;
+			}
+		}
+		.ip{
+			font-size: 44rpx;
+			color: #333;
+			padding: 20rpx 0 5rpx; 		// 上/左右/下
+		}
+		.address{
+			font-size: 28rpx;
+			color: #aaa
+		}
+	}
+	.section{
+		width: 690rpx;
+		margin: 50rpx auto;
+		border: 1px solid #eee;
+		border-radius: 10rpx;
+		box-shadow: 0 0 30rpx rgba(0,0,0,0.05);				// 阴影
+		.list{
+			.row{
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				padding: 0 30rpx;
+				height: 100rpx;
+				border-bottom: 1rpx solid #eee;
+				&:last-child{border-bottom: 0}
+				.left{
+					display: flex;
+					align-items: center;
+					.text{
+						padding-left: 20rpx;
+						color: #666;
+					}
+				}
+				.right{
+					display: flex;
+					align-items: center;
+					.text{
+						font-size: 28rpx;
+						padding-right: 20rpx;
+						color: #aaa;
+					}
+				}
+			}
+		}
+	}
+}
 
 </style>
