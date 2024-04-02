0040a3cab7cd3bbc81ed3a9949e26c279b3826cb
Narglc <qingcheliuzhi@qq.com>
2024-04-02 11:15:23 +0800

    壁纸项目:实现第一个图片轮播器swiper

---
+++ wallpaper-kt/pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="homeLayout">
+		<view class="banner">
+			<swiper circular indicator-dots indicator-color="rgba(255,255,255,0.5)" 
+			indicator-active-color="#FFF" autoplay>
+				<swiper-item v-for="(item,index) in banners" :key=index>
+						<image :src="item" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="notice">
+			<view class="left"></view>
+			<view class="center"></view>
+			<view class="right"></view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const banners = [
+	"../../common/images/banner1.jpg",
+	"../../common/images/banner2.jpg",
+	"../../common/images/banner3.jpg"
+]
+
+
+</script>
+
+<style lang="scss" scoped>
+	
+.homeLayout{
+	.banner{
+		width: 750rpx;
+		padding: 30rpx 0;
+		swiper{
+			width: 750rpx;
+			height: 340rpx;
+			swiper-item{
+				width: 100%;
+				height: 100%;
+				padding: 0 30rpx;
+				image{
+					width: 100%;
+					height: 100%;
+					border-radius: 10rpx;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ wallpaper-kt/pages/user/user.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="">
+		用户中心
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
